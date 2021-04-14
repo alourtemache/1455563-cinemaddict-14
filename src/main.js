84e5e99cbf196b52afb53a2b91f2a6c070c846c3
Алина <linaourtemache@gmail.com>
2021-04-14 08:16:23 +0300

    Импорт модулей

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createMovieCard} from './view/movie-card.js';
+import {createShowMoreButton} from './view/button-show-more.js';
+import {createMoreInfo} from './view/more-info.js';
+import {createProfileRating} from './view/profile-rating.js';
+
+const CARD_COUNT = 5;
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = siteMainElement.querySelector('.header');
+
+render(siteHeaderElement, createSiteMenuTemplate(), 'beforeend');
+render(siteHeaderElement, createMovieInfo(), 'beforeend');
+render(siteHeaderElement, createProfileRating(), 'beforeend');
+
+const filmCardElement = siteMainElement.querySelector('.film-list');
+const movieCard = filmCardElement.querySelector('.film-list__container');
+
+render(movieCard, createMovieCard(), 'beforeend');
+
+for (let i = 0; i < CARD_COUNT; i++) {
+  render(movieCard, createMovieCard(), 'beforeend');
+}
+
+render(filmCardElement, createShowMoreButton(), 'beforeend');
