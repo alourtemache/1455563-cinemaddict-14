48298a2f886e925e6829a0e0b15c770c26e4376c
Алина <linaourtemache@gmail.com>
2021-04-19 15:25:29 +0300

    Улучшено

--- src/main.js
+++ src/main.js
@@ -9,31 +9,36 @@ const CARD_COUNT = 5;
 const TOP_RATED = 2;
 const MOST_COMMENTED = 2;
 
-//Ставим HTML-код в указанное место
+// Ставим HTML-код в указанное место
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+const bodyElement = document.querySelector('body');
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
 
 render(siteMainElement, createSiteMenuTemplate(), 'beforeend');
 render(siteMainElement, createMoreInfo(), 'beforeend');
-render(siteHeaderElement, createProfileRating(), 'beforeend');
 render(siteMainElement, createFilmList(), 'beforeend');
+render(siteHeaderElement, createProfileRating(), 'beforeend');
 
 const filmListElement = siteMainElement.querySelector('.films-list__container');
-
+const filmListTopRatedElement = siteMainElement.querySelector('.films-list__top-rated .films-list__container');
+const filmListMostCommentedElement = siteMainElement.querySelector('.films-list__most-commented .films-list__container');
 
 for (let i = 0; i < CARD_COUNT; i++) {
   render(filmListElement, createMovieCard(), 'beforeend');
 }
+
+// Кнопка
+render(filmListElement, createShowMoreButton(), 'beforeend');
+
 for (let i = 0; i < TOP_RATED; i++) {
-  render(filmListElement, createMovieCard(), 'beforeend');
+  render(filmListTopRatedElement, createMovieCard(), 'beforeend');
 }
 for (let i = 0; i < MOST_COMMENTED; i++) {
-  render(filmListElement, createMovieCard(), 'beforeend');
+  render(filmListMostCommentedElement, createMovieCard(), 'beforeend');
 }
 
-//Кнопка
-render(filmListElement, createShowMoreButton(), 'beforeend');
+render(bodyElement, createShowMoreButton(), 'beforeend');
